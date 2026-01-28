2a2ef17c2ce4add39a38e0e523bbde3bf7a2a7a3
wilma <mornlitoralis@gmail.com>
2026-01-28 21:01:19 +0700

    config route

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,12 @@ Route::get('/pegawai', function(){
 });
 
 Route::get('/hitung', [App\Http\Controllers\segitigaController::class, 'perkalian']);
+
+use App\Http\Controllers\barangController;
+Route::get('barang',[barangController::class, 'index']) ->name('barang.index');
+Route::get('barang/create',[barangController::class, 'create']) ->name('barangs.create');
+Route::get('barang',[barangController::class, 'store']) ->name('barang.store');
+Route::get('barang/{id}/edit',[barangController::class, 'edit']) ->name('barang.edit');
+Route::get('barang/{id}',[barangController::class, 'update']) ->name('barang.update');
+Route::get('barang/{id}',[barangController::class, 'destroy']) ->name('barang.destroy');
+?>
