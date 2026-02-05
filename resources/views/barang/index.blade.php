0d07b6a830fea630a6373692ae3847cfd718cad0
wilma <mornlitoralis@gmail.com>
2026-02-05 13:18:15 +0700

    some changes on index blade

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -6,6 +6,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <h3>List Barang</h3>
+    <div class="wrapper">
+        <div class="container">
+            <div class="main-content">
+                <h3 class="mt-4">List barang</h3>
+                <table class="table table-stripped table-brodered">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>SKU</th>
+                            <th>Name barang</th>
+                            <th>Harga</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($barangs as $barang)
+                        <tr>
+                            <td>{{$barang['id']}}</td>
+                            <td>{{$barang['sku']}}</td>
+                            <td>{{$barang['namaBarang']}}</td>
+                            <td>{{$barang['harga']}}</td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="4">Empty</td>
+                        </tr>
+                        @endforelse
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
