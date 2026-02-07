21b90bfaffc0f59d3e8d282b823ca3464011d95c
wilma <mornlitoralis@gmail.com>
2026-02-07 20:22:38 +0700

    some changes

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -5,6 +5,13 @@
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
                 <h3>Web Pemesanan Barang</h3>
+                <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+                    <li>
+                        <a href="#" class="nav-link text-secondary">
+                            <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"/>Home</svg>
+                        </a>
+                    </li>
+                </ul>
             </a>
         </div>
     </div>
