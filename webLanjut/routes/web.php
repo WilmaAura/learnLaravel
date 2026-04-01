3e1fded878807d5a3659d6a42c78d8579eb0000b
wilma <mornlitoralis@gmail.com>
2026-04-01 09:38:23 +0700

    some changes on routes

--- webLanjut/routes/web.php
+++ webLanjut/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use app\Http\Controllers\ProdukController;
+use app\Http\Controllers\TransaksiController;
 Route::get('/', function () {
     return view('v_home');
 });
+
+Route::get("/produk", [ProdukController::class, 'index']);
+Route::get("/keranjang", [TransaksiController::class, 'index']);
+?>
+
