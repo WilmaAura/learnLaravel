5ca403c5df4d6ee504abe842f7c5ba88733459f5
wilma <mornlitoralis@gmail.com>
2026-04-08 07:40:33 +0700

    some changes and add files/folder

--- webLanjut/routes/web.php
+++ webLanjut/routes/web.php
@@ -3,11 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\TransaksiController;
+
 Route::get('/', function () {
     return view('v_home');
 });
 
-Route::get("/produk", [ProdukController::class, 'index']);
-Route::get("/keranjang", [TransaksiController::class, 'index']);
+Route::get("/v_produk", [ProdukController::class, 'index']);
+Route::get("/v_keranjang", [TransaksiController::class, 'index']);
 ?>
 
