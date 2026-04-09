2b7a701bc6b70c88492095e281d3b0b90027d165
wilma <mornlitoralis@gmail.com>
2026-04-09 12:00:18 +0700

    finishing all

---
+++ tugas/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\TransaksiController;
+
+Route::get('/', function () {
+    return view('v_home');
+});
+
+Route::get("/v_produk", [ProdukController::class, 'index']);
+Route::get("/v_keranjang", [TransaksiController::class, 'index']);
+?>
+
--- webLanjut/resources/views/v_produk.blade.php
+++ webLanjut/resources/views/v_produk.blade.php
@@ -1,1 +1,22 @@
-ini product
+@extends('layout')
+
+@section('content')
+<div class="pagetitle">
+  <h1>Data Tables</h1>
+  <nav>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="/">Home</a></li>
+      <li class="breadcrumb-item active">Produk</li>
+    </ol>
+  </nav>
+</div>
+
+<section class="section">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Produk</h5>
+      <p>ini halaman produk</p>
+    </div>
+  </div>
+</section>
+@endsection
