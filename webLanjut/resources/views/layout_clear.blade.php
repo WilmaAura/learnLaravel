b0cb5ecc54a051443e4d044b54f99c6cf4a98500
wilma <mornlitoralis@gmail.com>
2026-04-18 09:09:55 +0700

    add login

---
+++ webLanjut/resources/views/layout_clear.blade.php
@@ -0,0 +1,56 @@
+@php
+    // Logika untuk judul halaman berdasarkan URI
+    $hlm = request()->path() == '/' ? 'Home' : ucwords(str_replace('-', ' ', request()->path()));
+@endphp
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Toko - {{ $hlm }}</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <link href="{{ asset('NiceAdmin/assets/img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('NiceAdmin/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <link href="{{ asset('NiceAdmin/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('NiceAdmin/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('NiceAdmin/assets/css/style.css') }}" rel="stylesheet">
+</head>
+
+<body> 
+    <main>
+        {{-- Tempat konten utama akan muncul --}}
+        @yield('main') 
+    </main>
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
+        <i class="bi bi-arrow-up-short"></i>
+    </a>
+
+    <script src="{{ asset('NiceAdmin/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/chart.js/chart.umd.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/echarts/echarts.min.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/quill/quill.min.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ asset('NiceAdmin/assets/vendor/php-email-form/validate.js') }}"></script>
+
+    <script src="{{ asset('NiceAdmin/assets/js/main.js') }}"></script>
+</body>
+
+</html>
