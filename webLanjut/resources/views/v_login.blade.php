4d3141264b9a60436bed394b16a1f3b43c4f5306
wilma <mornlitoralis@gmail.com>
2026-04-19 08:19:58 +0700

    fix login section from unwritten method at form class

--- webLanjut/resources/views/v_login.blade.php
+++ webLanjut/resources/views/v_login.blade.php
@@ -2,25 +2,6 @@
 @section('content')
  <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
         <div class="container">
-          <div class="col-12">
-            <label for="username" class="form-label">username</label>
-            <input 
-            type="text"
-            name="username"
-            id="username"
-            class="form-control"
-            value="{{old ('username') }}"
-            requred>
-          </div>
-          <div class="col-12">
-            <label for= "password" class = "form-label">Password</label>
-            <input 
-            type ="password" 
-            name="password"
-            id="password"
-            class="form-control"
-            required>
-          </div>
           <div class="row justify-content-center">
             <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
@@ -39,19 +20,15 @@
                     <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
                     <p class="text-center small">Enter your username & password to login</p>
                   </div>
-                  @if (session('failed'))
-                    <div class="alert alert-danger small">
-                        {{session('failed')}}
-                     </div>
-                    @if (session('filed'))
+                    @if (session('failed'))
                       <div class="col-12 alert alert-danger" role="alert">
                         <hr>
                         <p class="mb-0">
-                          {session{'failed'}}    
+                          {{ session('failed') }}
                         </p>
                       </div>
                     @endif
-                  <form class="row g-3 needs-validation" novalidate>
+                  <form method="POST" action="{{ route('login') }}" class="row g-3 needs-validation" novalidate>
                       @csrf
                     <div class="col-12">
                       <label for="yourUsername" class="form-label">Username</label>
